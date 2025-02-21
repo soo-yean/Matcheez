84f090a6f197cc3910ba63be03494a6d3112bb54
soo-yean <sooyean.jang@gmail.com>
2025-02-21 13:16:01 +0900

    INSTALL: HeroUI

---
+++ src/app/members/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Members() {
+  return (
+    <div>
+      <h3 className="text-3xl">members page goes here...</h3>
+      <Link href="/">Go back home</Link>
+    </div>
+  );
+}
