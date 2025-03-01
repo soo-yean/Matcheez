41fa2afb9d4e9dbd95690a0738d090a883fa0025
soo-yean <sooyean.jang@gmail.com>
2025-03-01 17:21:50 +0900

    ADD feature: login validation

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { useForm } from "react-hook-form";
 import { Button, Card, CardBody, CardHeader, Input } from "@heroui/react";
 import { GiSeatedMouse } from "react-icons/gi";
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm();
+
+  //todo: data type
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <Card className="w-2/5 mx-auto">
       <CardHeader className="flex felx-col items-center justify-center">
@@ -16,11 +28,29 @@ export default function LoginForm() {
         </div>
       </CardHeader>
       <CardBody>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-4">
-            <Input label="Email" variant="bordered" />
-            <Input label="Password" variant="bordered" type="password" />
-            <Button fullWidth color="warning" type="submit">
+            <Input
+              label="Email"
+              variant="bordered"
+              {...register("email", { required: "Email is required" })}
+              isInvalid={!!errors.email} //!! for convert object to boolean
+              errorMessage={errors.email?.message as string}
+            />
+            <Input
+              label="Password"
+              variant="bordered"
+              type="password"
+              {...register("password", { required: "Password is required" })}
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message as string}
+            />
+            <Button
+              // isDisabled={!isValid}
+              fullWidth
+              color="warning"
+              type="submit"
+            >
               Login
             </Button>
           </div>
