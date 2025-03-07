b7d9f616f8e0a751c9adaa89ea842d1c1d341f0f
soo-yean <sooyean.jang@gmail.com>
2025-03-07 16:11:49 +0900

    SET UP: auth configuration

---
+++ src/auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+const prisma = new PrismaClient();
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
