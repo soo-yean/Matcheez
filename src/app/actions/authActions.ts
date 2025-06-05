7c930d210e237ea5d2218ca809265b6058dfa360
soo-yean <sooyean.jang@gmail.com>
2025-06-05 13:32:47 +0900

    REFACTOR: server side validation

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -1,31 +1,42 @@
 "use server";
 import { prisma } from "@/lib/prisma";
 import { registerSchema, RegisterSchema } from "@/lib/schemas/registerSchema";
+import { ActionResult } from "@/types";
+import { User } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 //these codes will be executed on the server side:3
 
-export async function registerUser(data: RegisterSchema) {
-  //server side validation
-  const validated = registerSchema.safeParse(data);
+export async function registerUser(
+  data: RegisterSchema
+): Promise<ActionResult<User>> {
+  try {
+    //server side validation
+    const validated = registerSchema.safeParse(data);
 
-  if (!validated.success) {
-    return { error: validated.error.errors };
+    if (!validated.success) {
+      return { status: "error", error: validated.error.errors };
+    }
+
+    const { name, email, password } = validated.data;
+
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) return { status: "error", error: "User already exists" };
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash: hashedpassword,
+      },
+    });
+
+    return { status: "success", data: user };
+  } catch (error) {
+    console.log(error);
+    return { status: "error", error: "Something went wrong..." };
   }
-
-  const { name, email, password } = validated.data;
-
-  const hashedpassword = await bcrypt.hash(password, 10);
-
-  const existingUser = await prisma.user.findUnique({ where: { email } });
-
-  if (existingUser) return { error: "User already exists" };
-
-  return prisma.user.create({
-    data: {
-      name,
-      email,
-      passwordHash: hashedpassword,
-    },
-  }); //no await needed when returning function itself because it's async anyways! The clients are gonna receive promise regardless:D
 }
